7be50aae06a1263a542450f712f1c84c8f2e1695
Nina <Nina@Nina-PC>
2013-11-07 13:11:51 +0100

    Im Load insert für Kunde-Rolle eingefügt

--- shop/src/main/resources/sql/load.sql
+++ shop/src/main/resources/sql/load.sql
@@ -5,6 +5,10 @@ INSERT INTO kunde (id, nachname, vorname, seit, art, kategorie, familienstand, n
 INSERT INTO kunde (id, nachname, vorname, seit, art, kategorie, familienstand, newsletter, rabatt, umsatz, version, email, password, erzeugt, aktualisiert,geburtsdatum,geschlecht) VALUES (5,'Beni','Socke','01.01.2002','F',1,null,1,'0,2',10000.00,1,'beni@hska.de','1','01.08.2006 00:00:00','01.08.2006 02:00:00','19.02.1989','M');
 INSERT INTO kunde (id, nachname, vorname, seit, art, kategorie, familienstand, newsletter, rabatt, umsatz, version, email, password, erzeugt, aktualisiert,geburtsdatum,geschlecht) VALUES (6,'Isabella','Kiwi','01.01.2001','P',1,'L',1,'0,2',870.00,1,'isi@hska.de','1','01.08.2006 00:00:00','01.08.2006 02:00:00','12.08.1990','W');
 
+--kunde-rolle
+INSERT INTO kunde_rolle (kunde_fk, rolle) VALUES (6, 'mitarbeiter');
+INSERT INTO kunde_rolle (kunde_fk, rolle) VALUES (5, 'kunde');
+
 --kunde-hobby
 INSERT INTO kunde_hobby (kunde_fk, hobby) VALUES (6,'L');
 
