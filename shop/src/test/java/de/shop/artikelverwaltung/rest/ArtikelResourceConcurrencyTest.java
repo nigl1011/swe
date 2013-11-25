0d3fbe0047a668276e414d18219f368cb8d77004
Nina <Nina@Nina-PC>
2013-11-25 18:00:04 +0100

    Bezeichung geändert

--- shop/src/test/java/de/shop/artikelverwaltung/rest/ArtikelResourceConcurrencyTest.java
+++ shop/src/test/java/de/shop/artikelverwaltung/rest/ArtikelResourceConcurrencyTest.java
@@ -34,38 +34,42 @@ import de.shop.util.HttpsConcurrencyHelper;
 public class ArtikelResourceConcurrencyTest extends AbstractResourceTest {
         private static final Logger LOGGER = Logger.getLogger(MethodHandles.lookup().lookupClass().getName());
 
-        private static final long TIMEOUT = 20;
+        private static final long TIMEOUT = 5;
 
         private static final Long ARTIKEL_ID_UPDATE = Long.valueOf(302);
-        private static final String NEUE_BEZEICHNUNG_A = "Bett A";
-        private static final String NEUE_BEZEICHNUNG_B = "Bett B";
+        private static final String NEUE_BEZEICHNUNG = "Bett";
+        private static final String NEUE_BEZEICHNUNG_2 = "Tisch";
 
         @Test
-        @InSequence(50)
+        @InSequence(1)
         public void updateUpdate() throws InterruptedException, ExecutionException, TimeoutException {
                 LOGGER.finer("BEGINN");
 
                 // Given
                 final Long artikelId = ARTIKEL_ID_UPDATE;
-                final String neueBezeichnungA = NEUE_BEZEICHNUNG_A;
-                final String neueBezeichnungB = NEUE_BEZEICHNUNG_B;
+                final String neueBezeichnung = NEUE_BEZEICHNUNG;
+                final String neueBezeichnung2 = NEUE_BEZEICHNUNG_2;
 
                 // When
                 Response response = getHttpsClient().target(ARTIKEL_ID_URI)
                                 .resolveTemplate(ARTIKEL_ID_PATH_PARAM, artikelId)
-                                .request().accept(APPLICATION_JSON)
+                                .request()
+                                .accept(APPLICATION_JSON)
                                 .get();
 
                 final Artikel artikel = response.readEntity(Artikel.class);
 
-                artikel.setBezeichnung(neueBezeichnungB);
+                artikel.setBezeichnung(neueBezeichnung2);
 
                 final Callable<Integer> concurrentUpdate = new Callable<Integer>() {
                         @Override
                         public Integer call() {
                                 final Response response = new HttpsConcurrencyHelper()
                                 .getHttpsClient(USERNAME_ADMIN, PASSWORD_ADMIN)
-                                .target(ARTIKEL_URI).request().accept(APPLICATION_JSON).put(json(artikel));
+                                .target(ARTIKEL_URI)
+                                .request()
+                                .accept(APPLICATION_JSON)
+                                .put(json(artikel));
                                 
                                 final int status = response.getStatus();
                                 response.close();
@@ -74,13 +78,17 @@ public class ArtikelResourceConcurrencyTest extends AbstractResourceTest {
                 };
                 
                 final Integer status = Executors.newSingleThreadExecutor()
-                								.submit(concurrentUpdate).get(TIMEOUT, SECONDS);
+                								.submit(concurrentUpdate)
+                								.get(TIMEOUT, SECONDS);
                
                 assertThat(status.intValue()).isEqualTo(HTTP_OK);
 
-                artikel.setBezeichnung(neueBezeichnungA);
-                response = getHttpsClient(USERNAME_ADMIN, PASSWORD_ADMIN).target(ARTIKEL_URI).request()
-                                .accept(APPLICATION_JSON).put(json(artikel));
+                artikel.setBezeichnung(neueBezeichnung);
+                response = getHttpsClient(USERNAME_ADMIN, PASSWORD_ADMIN)
+                				.target(ARTIKEL_URI)
+                				.request()
+                                .accept(APPLICATION_JSON)
+                                .put(json(artikel));
 
                 // Then
                 assertThat(response.getStatus()).isEqualTo(HTTP_CONFLICT);
